c7f2cfbd0bb53f6b5b0895da36b4d610a21f0bd5
Daniil <dr.chashka@gmail.com>
2020-07-11 02:31:12 +0300

    Symfony project created

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// Bot token: 861121918:AAE1caaPhjPytqAhgEWdXaG9azEQIyVmcJs
 const TOKEN = '861121918:AAE1caaPhjPytqAhgEWdXaG9azEQIyVmcJs';
 $url = 'https://api.telegram.org/bot' . TOKEN . '/getUpdates';
 $response = file_get_contents($url);
