b2c3442bc80c10d347a5b01f3181b9b737fbd226
Daniil <dr.chashka@gmail.com>
2020-07-10 23:30:54 +0300

    Project starting

---
+++ home.php
@@ -0,0 +1,7 @@
+<?php
+// Bot token: 861121918:AAE1caaPhjPytqAhgEWdXaG9azEQIyVmcJs
+const TOKEN = '861121918:AAE1caaPhjPytqAhgEWdXaG9azEQIyVmcJs';
+$url = 'https://api.telegram.org/bot' . TOKEN . '/getUpdates';
+$response = file_get_contents($url);
+echo $response;
+var_dump($response);
