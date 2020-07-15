b34344138e94509902d6ebee40fd9892d9ca2aee
Daniil <dr.chashka@gmail.com>
2020-07-15 03:40:09 +0300

    Trying to understend why transcription doesn't print

---
+++ test.php
@@ -0,0 +1,81 @@
+<?php
+getWordInfo(path);
+
+function getWordInfo(string $word): ?array
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://lingua-robot.p.rapidapi.com/language/v1/entries/en/$word",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "x-rapidapi-host: lingua-robot.p.rapidapi.com",
+            "x-rapidapi-key: 99d6039723mshcaa6930780810d7p173043jsna4ae1d23e3de"
+        )
+    ));
+
+    $response = curl_exec($curl);
+    $curlError = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($curlError)
+    {
+        echo "cURL Error #:" . $err;
+    }
+    else
+    {
+        $response = json_decode($response, true);
+
+        $entries = $response["entries"][0];
+        //Если слово существует, то получить данные
+        if (!empty($entries))
+        {
+            $pronunciations = getPronunciations($entries);
+            return $pronunciations;
+        }
+    }
+}
+
+//Функция получает транкрипции и аудио английского и американского произношений слова
+function getPronunciations(array $entries): ?array
+{
+    $pronunciations = $entries["pronunciations"];
+
+    foreach ($pronunciations as $pronunciation)
+    {
+        if ($pronunciation["context"]["regions"][0] === "United Kingdom")
+        {
+            $transcriptionUK = $pronunciation["transcriptions"]["transcription"];
+            var_dump($transcriptionUK);
+            if (isset($pronunciation["audio"]))
+            {
+                $audioUK = $pronunciation["audio"]["url"];
+            }
+        }
+        elseif ($pronunciation["context"]["regions"][0] === "United States")
+        {
+            $transcriptionUS = $pronunciation["transcriptions"]["transcription"];
+            if (isset($pronunciation["audio"]))
+            {
+                $audioUS = $pronunciation["audio"]["url"];
+            }
+
+            break;
+        }
+    }
+
+    return array(
+        "transcriptionUK" => $transcriptionUK,
+        "transcriptionUS" => $transcriptionUS,
+        "audioUK" => $audioUK,
+        "audioUS" => $audioUS
+    );
+}
+?>
