cbedbb6c3322e6ddb9765fb6e89369c4625e31dd
Daniil <dr.chashka@gmail.com>
2020-07-18 23:49:39 +0300

    Test

--- index.php
+++ index.php
@@ -24,6 +24,8 @@ if (!empty($callbackQuery))
 
         $definitionsByPartOfSpeech = $tempWordInfo["definitionsByPartOfSpeech"];
         $tempWordInfo = null;
+        
+        var_dump($definitionsByPartOfSpeech);
 
         foreach ($definitionsByPartOfSpeech as $key => $value)
         {
@@ -38,7 +40,7 @@ if (!empty($callbackQuery))
 
         $reply = "What part of speech is your word?";
 
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply_markup, 'reply_markup' => $reply_markup ]);
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     }
 }
 else
