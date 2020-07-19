fc505fe61f1d650c966bdc58ffd035fbb068889a
Daniil <dr.chashka@gmail.com>
2020-07-19 03:15:43 +0300

    Canceling refactoring

--- getWordInfo.php
+++ getWordInfo.php
@@ -3,24 +3,10 @@
 
 function getWordInfo(string $word): ?array
 {
-    //Заменяю пробел на %2520, чтобы запрос фраз проходил корректно
+    //Заменяю пробел на %20, чтобы запрос фраз проходил корректно
     $word = str_replace(' ', '%20', $word);
 
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, "https://private-anon-5ac36e7a1c-linguarobot.apiary-mock.com/language/v1/entries/en/$word");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "X-RapidAPI-Key: 99d6039723mshcaa6930780810d7p173043jsna4ae1d23e3de",
-        "Accept: application/json"
-    ));
-
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    /*$curl = curl_init();
+    $curl = curl_init();
 
     curl_setopt_array($curl, array(
         CURLOPT_URL => "https://lingua-robot.p.rapidapi.com/language/v1/entries/en/$word",
@@ -69,28 +55,6 @@ function getWordInfo(string $word): ?array
                 "wordIsCorrect" => false
             );
         }
-    }*/
-
-    $response = json_decode($response, true);
-
-    $entries = $response["entries"][0];
-    //Если слово существует, то получить данные
-    if (!empty($entries))
-    {
-        $pronunciations = getPronunciations($entries);
-        $definitionsByPartOfSpeech = getDefinitionsByPartOfSpeech($entries);
-
-        return array(
-            "wordIsCorrect" => true,
-            "pronunciations" => $pronunciations,
-            "definitionsByPartOfSpeech" => $definitionsByPartOfSpeech
-        );
-    }
-    else
-    {
-        return array(
-            "wordIsCorrect" => false
-        );
     }
 }
 
