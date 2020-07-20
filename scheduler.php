a148a6d53910f67f998f844b5ee3247c2876a7b1
Daniil <dr.chashka@gmail.com>
2020-07-20 18:48:12 +0300

    Update scheduler.php
    Working scheduler  was developed

--- scheduler.php
+++ scheduler.php
@@ -3,14 +3,18 @@ require_once 'config.php';
 require_once 'vendor/autoload.php';
 use Telegram\Bot\Api;
 
-$db = new MysqliDb (DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
 $telegram = new Api(TG_BOT_TOKEN);
-
-$chatId = $result["message"]["chat"]["id"];
 
 $inlineKeyboard = [[[ 'text' => "Learn words", 'callback_data' => "learn" ]]];
 $keyboard = [ 'inline_keyboard' => $inlineKeyboard ];
 $reply_markup = json_encode($keyboard);
 
-$telegram->sendMessage([ 'chat_id' => $chatId, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+$db = new MysqliDb(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+$chatIds = $db->get('users_data', null, 'chat_id');
+foreach ($chatIds as $chatId)
+{
+    $telegram->sendMessage([ 'chat_id' => $chatId, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+}
+
+
