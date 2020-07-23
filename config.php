7d03672c2a9564343c090ace3df66d47d5daa1f2
Daniil <dr.chashka@gmail.com>
2020-07-23 09:20:05 +0300

    Bug fix

--- config.php
+++ config.php
@@ -4,15 +4,15 @@ const TG_BOT_TOKEN = '861121918:AAE1caaPhjPytqAhgEWdXaG9azEQIyVmcJs';
 //GetWordInfo constants
 const LINGUA_ROBOT_API_KEY = '99d6039723mshcaa6930780810d7p173043jsna4ae1d23e3de';
 //Database constants
-const DB_HOST = 'localhost';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = 'BJx@3~rQ_N98%tn';
-const DB_NAME = 'telegram_bot_db';
+//const DB_HOST = 'localhost';
+//const DB_USERNAME = 'root';
+//const DB_PASSWORD = 'BJx@3~rQ_N98%tn';
+//const DB_NAME = 'telegram_bot_db';
 
-//const DB_HOST = 'eu-cdbr-west-03.cleardb.net';
-//const DB_USERNAME = 'bdb1f0ef971da9';
-//const DB_PASSWORD = '1e6b264d';
-//const DB_NAME = 'heroku_e17492b76c5180a';
+const DB_HOST = 'eu-cdbr-west-03.cleardb.net';
+const DB_USERNAME = 'bdb1f0ef971da9';
+const DB_PASSWORD = '1e6b264d';
+const DB_NAME = 'heroku_e17492b76c5180a';
 //Parts of speech
 const PART_OF_SPEECH_NOUN = 'noun';
 const PART_OF_SPEECH_VERB = 'verb';
