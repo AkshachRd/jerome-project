8331570af45e5c8a76feceb8dc7849debcf4d0e7
Daniil <dr.chashka@gmail.com>
2020-08-11 00:56:19 +0300

    Key constants were moved to the Heroku env. varibles config

--- config.php
+++ config.php
@@ -1,16 +1,13 @@
 <?php
 //Telegram API constant
-const TG_BOT_TOKEN = '861121918:AAE1caaPhjPytqAhgEWdXaG9azEQIyVmcJs';
+//Heroku env. variables
 //GetWordInfo constants
-const LINGUA_ROBOT_API_KEY = '99d6039723mshcaa6930780810d7p173043jsna4ae1d23e3de';
+//Heroku env. variables
 //Emoji
 const EMOJI_UK_FLAG = "\xF0\x9F\x87\xAC\xF0\x9F\x87\xA7";
 const EMOJI_US_FLAG = "\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8";
 //Database constants
-const DB_HOST = 'eu-cdbr-west-03.cleardb.net';
-const DB_USERNAME = 'bdb1f0ef971da9';
-const DB_PASSWORD = '1e6b264d';
-const DB_NAME = 'heroku_e17492b76c5180a';
+//Heroku env. variables
 //Parts of speech
 const PART_OF_SPEECH_NOUN = 'noun';
 const PART_OF_SPEECH_VERB = 'verb';
